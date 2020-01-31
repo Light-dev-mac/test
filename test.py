6148e42ddd833af7374eacf3420b4b1f9c8d88a0
yuhori75 <berliner.philharmoniker.karajan@gmail.com>
2020-01-31 19:34:58 +0900

    END

--- test.py
+++ test.py
@@ -1,3 +1,27 @@
 """
 このファイルに解答コードを書いてください
 """
+
+
+
+f =  open('input.txt', "rt")
+str_text = f.read()
+str_text_lines = str_text.split('\n')
+m = int(str_text_lines[-2])
+pairs = str_text_lines[:-2]
+dict_pairs = {}
+
+for pair in pairs:
+    i, s  = pair.split(':')
+    dict_pairs[int(i)] = s
+
+dict_pairs = sorted(dict_pairs.items())
+is_matched = False
+for pair in dict_pairs:
+    i, s = pair
+    if m % i == 0:
+        is_matched = True
+        print(s, end='')
+
+if not is_matched:
+    print(m)
